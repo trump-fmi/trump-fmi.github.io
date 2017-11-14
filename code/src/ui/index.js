b70fa1a847c35c37d7b70288f2f063463ad40cee
Julian <julian.sudendorf@gmail.com>
2017-11-14 16:06:22 +0100

    update tag fields to use typeahead to asist user

--- code/src/ui/index.js
+++ code/src/ui/index.js
@@ -1,3 +1,18 @@
+function typeaheadField(title, url){
+
+  return {
+  // "ui:title": "Your password",
+  // "ui:description": "The best password",
+  "ui:field": "asyncTypeahead",
+  "asyncTypeahead": {
+     "url": url,
+      search: (url, query) => fetch(`${url}&query=${query}`).then(res => res.json()).then(res => res.data.map(el => el.key)),
+      placeholder: title
+   }
+};
+
+}
+
 const ui = {
   label_split: {
     split_chars: {
@@ -7,17 +22,22 @@ const ui = {
     }
   },
   mapping: {
-    level : {
       constraints: {
-        tag: {
-          "ui:field": "typeahead",
-          "typeahead": {
-             "options": [ { "state": "New York" }, { "code": "Washington" }],
-             "labelKey": "state"
-           }
+        items: {
+          tag: typeaheadField('Tag', "https://taginfo.openstreetmap.org/api/4/keys/all?sortname=count_all&sortorder=desc&page=1&rp=17"),
+          // equals: typeaheadField('equals', 'https://taginfo.openstreetmap.org/api/4/key/values?sortname=count_all&sortorder=desc&page=1&rp=10')
+        }
+      },
+      sublevels: {
+        items: {
+          constraints: {
+            items: {
+              tag: typeaheadField('Tag', "https://taginfo.openstreetmap.org/api/4/keys/all?sortname=count_all&sortorder=desc&page=1&rp=17"),
+              // equals: typeaheadField('equals', 'https://taginfo.openstreetmap.org/api/4/key/values?sortname=count_all&sortorder=desc&page=1&rp=10')
+            }
+          }
         }
       }
-    }
   }
 };
 
